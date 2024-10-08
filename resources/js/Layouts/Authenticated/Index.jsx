af968be4ea8a0feac53e8a94d1439084c12f3990
Naufal Imtiyaz <naufalimtiyaz35@gmail.com>
2024-10-08 16:59:30 +0700

    feat: integration dashboard

--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Sidebar from "@/Layouts/Authenticated/Sidebar";
 import Topbar from "@/Layouts/Authenticated/Topbar";
 
-export default function Authenticated({ children }) {
+export default function Authenticated({ user, children }) {
     return (
         <>
             <div className="mx-auto max-w-screen hidden lg:block">
@@ -11,7 +11,7 @@ export default function Authenticated({ children }) {
                 {/* START: Content */}
                 <div className="ml-[300px] px-[50px]">
                     <div className="py-10 flex flex-col gap-[50px]">
-                        <Topbar/>
+                        <Topbar user={user}/>
                         <main>{children}</main>
                     </div>
                 </div>
